8a3c42910fb1acc750e1e7c9fe27aa5f8af62045
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-12-06 09:30:13 -0700

    formated cityreader so it shows name, lat, lon on print

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,9 +1,13 @@
+import csv
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 class City:
-  def __init__(self, lat, lon):
+  def __init__(self, name, lat, lon):
+    self.name = name
     self.lat = lat
     self.lon = lon
+  def __repr__(self):
+    return f"name: {self.name}, lat: {self.lat}, lon: {self.lon}"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -23,7 +27,10 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+  with open('cities.csv', newline='') as csvfile:
+    creader = csv.reader(csvfile, delimiter=',')
+    for x in creader:
+      cities.append(City(x[0], x[3], x[4]))
     return cities
 
 cityreader(cities)
